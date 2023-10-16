8ef74b9956998d9b3428c6059d77fb9a6ed46968
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-10-16 21:23:05 +0530

    Create more_accurate_delhi_lots.py

---
+++ more_accurate_delhi_lots.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import json
+
+# Load boundary coordinates for Delhi
+with open("new-delhi-coordinates.json", "r") as file:
+    data = json.load(file)
+geometry = data['features'][0]['geometry']
+lat_lon_pairs = geometry['coordinates'][0]
+
+# Function to check if a point is inside a polygon
+def is_point_inside_polygon(point, polygon):
+    x, y = point
+    odd_nodes = False
+    j = len(polygon) - 1  # Last vertex in the polygon
+    
+    for i in range(len(polygon)):
+        xi, yi = polygon[i]
+        xj, yj = polygon[j]
+        if yi < y and yj >= y or yj < y and yi >= y:
+            if xi + (y - yi) / (yj - yi) * (xj - xi) < x:
+                odd_nodes = not odd_nodes
+        j = i
+
+    return odd_nodes
+
+# Load the CSV file into a pandas DataFrame
+df = pd.read_csv('rough_delhi_lots.csv')
+
+# Check if each point is inside Delhi and store the results in a new column
+df['inside_delhi'] = df.apply(lambda row: is_point_inside_polygon((row['longitude'], row['latitude']), lat_lon_pairs), axis=1)
+
+# Filter the points that are inside Delhi
+delhi_points = df[df['inside_delhi']]
+
+# Save filtered points to a new CSV file
+delhi_points.to_csv('more_accurate_delhi_lots.csv', index=False)
