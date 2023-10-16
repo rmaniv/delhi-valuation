da94afe29e672c0354f15e09e189cec9b8c02d46
vinamrsachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-10-17 00:43:06 +0530

    Update address_look_up.py

--- address_look_up.py
+++ address_look_up.py
@@ -5,6 +5,9 @@ import time
 # Load the CSV file
 df = pd.read_csv('more_accurate_delhi_lots.csv')
 
+# Add an empty 'Address' column to the DataFrame
+df['Address'] = ''
+
 # Define a function to reverse geocode using Nominatim API and append all addresses
 def reverse_geocode_nominatim(lat, lng, user_agent):
     base_url = "https://nominatim.openstreetmap.org/reverse"
@@ -25,31 +28,26 @@ def reverse_geocode_nominatim(lat, lng, user_agent):
         print(f"Error processing response: {e}")
         return None
 
-addresses = []
-
 user_agent_base = "DelhiLots"
 user_agent_counter = 1
 
-count = 0
 # Reverse geocode each row in the DataFrame
 for index, row in df.iterrows():
+
     if index % 100 == 0:  # Change user agent every 100 requests
         user_agent = user_agent_base + str(user_agent_counter)
         user_agent_counter += 1
 
     address = reverse_geocode_nominatim(row['latitude'], row['longitude'], user_agent)
-    if count % 1000 == 0:
-        print(count)
-    count += 1
-    if address:
-        addresses.append(address)
-    time.sleep(0.001)  # Respect Nominatim's rate limit
+    
+    # Update the 'Address' column for the current row
+    df.at[index, 'Address'] = address
+    print(df.at[index, 'Address'])
+    
+    time.sleep(1)
 
-# Convert list of addresses to a DataFrame
-df_addresses = pd.DataFrame(addresses, columns=["Address"])
+# Filter out rows without "Delhi" in the address
+df = df[df['Address'].str.contains('Delhi', case=False, na=False)]
 
-# Drop rows that don't contain the word "Delhi"
-df_addresses = df_addresses[df_addresses['Address'].str.contains('Delhi', case=False, na=False)]
-
-# Save the filtered DataFrame to CSV
-df_addresses.to_csv('all_addresses.csv', index=False)
+# Save the updated DataFrame to CSV
+df.to_csv('more_accurate_delhi_lots_with_addresses.csv', index=False)
