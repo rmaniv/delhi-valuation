3b8bd923942cb72eeb967c2be6662d2da26c587e
Vinamr Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2023-11-16 13:10:43 +0530

    Update and rename rough-delhi-lots.py to rough-delhi-buildings.py

--- rough-delhi-buildings.py
+++ rough-delhi-buildings.py
@@ -1,5 +1,5 @@
 import pandas as pd
-lots = pd.DataFrame()
+buildings = pd.DataFrame()
 
 # - divide 391_buildings.csv (look up README.md) into 2 files
 # - split 391_buildings.csv into 2 files to be able to work on them with a computer with 16GB of RAM.
@@ -8,12 +8,12 @@ lots = pd.DataFrame()
 chunksize = 10 ** 6
 file = 1
 for chunk in pd.read_csv('391_buildings_part1.csv', chunksize=chunksize):
-    file_name = 'lots/lots_' + str(file) + '.csv'
+    file_name = 'buildings/buildings_' + str(file) + '.csv'
     chunk.to_csv(file_name)
     print('File ', file, ' done.')
     file += 1
 for chunk in pd.read_csv('391_buildings_part2.csv', chunksize=chunksize):
-    file_name = 'lots/lots_' + str(file) + '.csv'
+    file_name = 'buildings/buildings_' + str(file) + '.csv'
     chunk.to_csv(file_name)
     print('File ', file, ' done.')
     file += 1
@@ -21,17 +21,17 @@ for chunk in pd.read_csv('391_buildings_part2.csv', chunksize=chunksize):
 # remove the first 2 useless columns
 
 for i in range(1, 75, 1):
-    file_name = 'lots/lots_' + str(i) + '.csv'
+    file_name = 'buildings/buildings_' + str(i) + '.csv'
     df = pd.read_csv(file_name)
     df = df.drop(df.columns[0], axis=1)
     df = df.drop(df.columns[0], axis=1)
 
-# extract lots falling roughly in Delhi based on manually selected coordinates
+# extract buildings falling roughly in Delhi based on manually selected coordinates
 
 delhi_ = pd.DataFrame()
 for i in range(1, 75, 1):
     
-    file_name = 'lots/lots_' + str(i) + '.csv'
+    file_name = 'buildings/buildings_' + str(i) + '.csv'
     df = pd.read_csv(file_name)
     
     latitude_mask = (df['latitude'] > 28.377060) & (df['latitude'] < 28.938108)
@@ -43,6 +43,6 @@ for i in range(1, 75, 1):
     
     delhi_ = pd.concat([delhi_, filtered_df])
 
-# save those lots in rough_delhi_lots.csv
+# save those buildings in rough_delhi_buildings.csv
 
-delhi_.to_csv('rough_delhi_lots.csv')
+delhi_.to_csv('rough_delhi_buildings.csv')
